57289b9ab36f6c32492214a89fa2221a40077760
= <=>
2022-07-18 16:17:51 +0300

    refactor ParDoGetMulti

--- dspardo/get_multi.go
+++ dspardo/get_multi.go
@@ -12,6 +12,44 @@ func ParDoGetMulti[T interface{}](
 	keys []*datastore.Key,
 	do func(ctx context.Context, worker int, entities []T) error,
 ) error {
+	consumers, keyBatches := startConsumers(ctx, c, do)
+	keyBatches, err := sendBatches(ctx, c, keys, keyBatches)
+
+	close(keyBatches)
+	if err != nil {
+		return err
+	}
+
+	if err = consumers.Wait(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func sendBatches(ctx context.Context, c *Client, keys []*datastore.Key, keyBatches chan []*datastore.Key) (chan []*datastore.Key, error) {
+	var err error
+	batchSize := c.batchSize
+	for offset := 0; offset < len(keys) && err == nil; offset += batchSize {
+		remaining := len(keys) - offset
+		if batchSize > remaining {
+			batchSize = remaining
+		}
+
+		select {
+		case keyBatches <- keys[offset : offset+batchSize]:
+		case <-ctx.Done():
+			err = ctx.Err()
+		}
+	}
+	return keyBatches, err
+}
+
+func startConsumers[T interface{}](
+	ctx context.Context,
+	c *Client,
+	do func(ctx context.Context, worker int, entities []T) error,
+) (*errgroup.Group, chan []*datastore.Key) {
 	var consumers errgroup.Group
 	keyBatches := make(chan []*datastore.Key, c.numWorkers)
 	for i := 0; i < c.numWorkers; i++ {
@@ -30,30 +68,5 @@ func ParDoGetMulti[T interface{}](
 			return nil
 		})
 	}
-
-	var err error
-	batchSize := c.batchSize
-	for offset := 0; offset < len(keys) && err == nil; offset += batchSize {
-		remaining := len(keys) - offset
-		if batchSize > remaining {
-			batchSize = remaining
-		}
-
-		select {
-		case keyBatches <- keys[offset : offset+batchSize]:
-		case <-ctx.Done():
-			err = ctx.Err()
-		}
-	}
-
-	close(keyBatches)
-	if err != nil {
-		return err
-	}
-
-	if err = consumers.Wait(); err != nil {
-		return err
-	}
-
-	return nil
+	return &consumers, keyBatches
 }
